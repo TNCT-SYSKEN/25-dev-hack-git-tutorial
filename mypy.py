2fb9f7d0b0c77eea2062148fbac90e57f51c6e99
AkaishiTakumi <115193708+AkaishiTakumi@users.noreply.github.com>
2026-06-15 17:17:52 +0900

    feat: テキストを出力するプログラムをいくつか追加
    * 以下のテキストを出力する。
    ```
    Hello World!
    Welcome to mypy.
    This is a simple example of a C program.
    You can use this as a starting point for your own C programs.
    Goodbye!
    This is the end of the program.
    ```

---
+++ mypy.py
@@ -0,0 +1,6 @@
+printf("Hello World!\n");
+printf("Welcome to mypy.\n");
+printf("This is a simple example of a C program.\n");
+printf("You can use this as a starting point for your own C programs.\n");
+printf("Goodbye!\n");
+printf("This is the end of the program.\n");
