1f4217e01b6fbd648a1ea82f1c8f79e1bf64684c
HPK5884 <kobashou0908@gmail.com>
2026-06-15 17:29:07 +0900

    feature/Cynomys/branch-zibuntest
    * Let's learn Python together!を出力
    * Python is a versatile programming language.を出力

--- hoge.py
+++ hoge.py
@@ -1,4 +1,6 @@
 print("Hello, World!")
 print("Welcome to Python programming!")
 print("This is a simple Python script.")
+print("Let's learn Python together!")
+print("Python is a versatile programming language.")
 #コミットメッセージは細かく書こう
